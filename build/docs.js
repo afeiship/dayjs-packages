a0a0159fca0fc5d4b5b173673f842210daf9a6ab
afeiship <1290657123@qq.com>
2022-01-04 09:13:56 +0800

    chore: use dart-sass for sass compile

--- build/docs.js
+++ build/docs.js
@@ -2,14 +2,13 @@
   'use strict';
 
   var gulp = require('gulp');
-  var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del'],
-  });
+  var sass = require('gulp-dart-sass');
+
   //styles
   gulp.task('docs', function () {
     return gulp
       .src('docs/style.scss')
-      .pipe($.sass({ outputStyle: 'expanded' }).on('error', $.sass.logError))
+      .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
       .pipe(gulp.dest('docs'));
   });
 })();
